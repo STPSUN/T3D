c9c51b1efdc9dca1efcb8dbcc75a815b61c2e99a
STPSUN <497432519@qq.com>
2018-10-01 00:17:52 +0800

    bug：ios注册按钮

--- web/database.php
+++ web/database.php
@@ -4,17 +4,17 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.74.236.198',
-//    'hostname'        => '127.0.0.1',
+//    'hostname'        => '47.74.236.198',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 't3d',
     // 用户名
-    'username'        => 't3d',
-//    'username'        => 'root',
+//    'username'        => 't3d',
+    'username'        => 'root',
 
     // 密码
-    'password'        => 'TKnmizdWp6',
-//    'password'        => 'root',
+//    'password'        => 'TKnmizdWp6',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
